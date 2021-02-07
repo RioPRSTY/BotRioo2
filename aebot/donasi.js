1713202bbf38db46a5b21c2be85771c6590d88a4
RioPRSTY <75353718+RioPRSTY@users.noreply.github.com>
2021-02-07 20:19:28 +0700

    Add files via upload

---
+++ aebot/donasi.js
@@ -0,0 +1,20 @@
+const donasi = (Ig, name) => { 
+	return 
+	 `     
+             𝗗𝗢𝗡𝗔𝗦𝗜  
+
+     *DONASI SEIKHLASNYA:)* 
+ 
+  *BOT BY ${name}*
+
+
+
+
+Note:
+Jika Gamau Donasi Gpp
+
+
+     `
+}
+
+exports.donasi = donasi
